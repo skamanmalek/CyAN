a58c8d06c5699681ce41f6a70779dde182557bb5
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 18:45:39 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -28,12 +28,12 @@ b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
 b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
 
 # User Input
-AVFST_Max_user = st.slider("AVFST_Max User Input", 67, 106, initial_values['AVFST_Max'])
-ARAIN_Average_user = st.slider("ARAIN_Average User Input", 0, 450, initial_values['ARAIN_Average'])
-HUC12_TN_user = st.slider("HUC12_TN User Input", 0, 500, initial_values['HUC12_TN'])
-HUC10_TP_user = st.slider("HUC10_TP User Input", 0, 50, initial_values['HUC10_TP'])
-HUC10_cropland_area_user = st.slider("HUC10_cropland_area User Input", 0, 100, initial_values['HUC10_cropland_area_1'])
-HUC12_developed_area_5_user = st.slider("HUC12_developed_area_5 User Input", 0, 100, initial_values['HUC12_developed_area_5'])
+AVFST_Max_user = st.slider("AVFST_Max User Input", b1, b2, initial_values['AVFST_Max'])
+ARAIN_Average_user = st.slider("ARAIN_Average User Input", c1, c2, initial_values['ARAIN_Average'])
+HUC12_TN_user = st.slider("HUC12_TN User Input", d1, d2, initial_values['HUC12_TN'])
+HUC10_TP_user = st.slider("HUC10_TP User Input", e1, e2, initial_values['HUC10_TP'])
+HUC10_cropland_area_user = st.slider("HUC10_cropland_area User Input", float(f1), float(f2), initial_values['HUC10_cropland_area_1'])
+HUC12_developed_area_5_user = st.slider("HUC12_developed_area_5 User Input", float(g1), float(g2), initial_values['HUC12_developed_area_5'])
 
 # Calculate Predicted Magnitude
 Y = coefficients['intercept'] + \
@@ -69,4 +69,4 @@ chart_data = pd.DataFrame({
     'Magnitude Type': ['Initial Bloom Magnitude', 'Predicted Bloom Magnitude'],
     'Magnitude Value': [initial_values['Norm_CyAN'], final_bloom_magnitude]
 })
-st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value')
+st.bar_chart(chart_data, x='Magnitude Type', y='
