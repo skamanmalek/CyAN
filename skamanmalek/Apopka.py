9f29c6d591b42e90761a3dff553a1b1915b6b655
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 18:56:20 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -28,13 +28,9 @@ b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
 b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
 
 # User Input
-# User Input for AVFST_Max
-AVFST_Max_user = st.slider("AVFST_Max User Input", min_value=67, max_value=106, value=initial_values['AVFST_Max'])
-ARAIN_Average_user = st.slider("ARAIN_Average User Input", 0, 450, initial_values['ARAIN_Average'])
-HUC12_TN_user = st.slider("HUC12_TN User Input", 0, 500, initial_values['HUC12_TN'])
-HUC10_TP_user = st.slider("HUC10_TP User Input", 0, 50, initial_values['HUC10_TP'])
-HUC10_cropland_area_user = st.slider("HUC10_cropland_area User Input", 0, 100, initial_values['HUC10_cropland_area_1'])
-HUC12_developed_area_5_user = st.slider("HUC12_developed_area_5 User Input", 0, 100, initial_values['HUC12_developed_area_5'])
+AVFST_Max_user = st.slider(AVFST_Max User Input, min_value=67, max_value=106, value==initial_values['AVFST_Max'], step=0.1)
+
+
 
 # Calculate Predicted Magnitude
 Y = coefficients['intercept'] + \
