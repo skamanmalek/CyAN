2027ef6a7a9af3447459addf00e201ddee35df34
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 14:54:47 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -89,10 +89,17 @@ final_bloom_magnitude = predicted_y1 * max_values['Norm_CyAN']
 # Calculate the percentage change
 percentage_change = ((final_bloom_magnitude - initial_values['Norm_CyAN']) / initial_values['Norm_CyAN']) * 100
 
+# Display the final result
+st.write(f"Initial Cyanobacteria Bloom Magnitude with the Baseline of 2022: {initial_values['Norm_CyAN']:.4f}")
+st.write(f"Predicted Cyanobacteria Bloom Magnitude: {final_bloom_magnitude:.4f}")
+st.write(f"Percentage Change: {percentage_change:.2f}%")
+
 # Display a message based on the change
 if percentage_change > 0:
+    st.success("The estimated bloom magnitude has increased.")
     st.info("The annual magnitude of cyanobacteria bloom is predicted to increase.")
 elif percentage_change < 0:
+    st.error("The estimated bloom magnitude has decreased.")
     st.info("The annual magnitude of cyanobacteria bloom is predicted to decrease.")
 else:
     st.info("The estimated bloom magnitude remains the same.")
