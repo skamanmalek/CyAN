aa33b1278c1017dc14f0389d6c993ffcb1411fb6
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 18:59:34 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -27,9 +27,13 @@ coefficients = {
 b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
 b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
 
+#Slider variables:
+b3, c1, d1, e1, f1, g1 = 67.00, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
+b4, c2, d2, e2, f2, g2 = 106.00, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
+
 
 # User Input
-AVFST_Max_user = st.slider("AVFST_Max User Input", b1, b2, initial_values['AVFST_Max'])
+AVFST_Max_user = st.slider("AVFST_Max User Input", b3, b4, initial_values['AVFST_Max'])
 ARAIN_Average_user = st.slider("ARAIN_Average User Input", c1, c2, initial_values['ARAIN_Average'])
 HUC12_TN_user = st.slider("HUC12_TN User Input", d1, d2, initial_values['HUC12_TN'])
 HUC10_TP_user = st.slider("HUC10_TP User Input", e1, e2, initial_values['HUC10_TP'])
