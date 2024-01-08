f79d5c494da84eb89f85fe4eac06bc67f3b14c30
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 19:01:47 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -28,15 +28,15 @@ b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
 b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
 
 #Slider variables:
-b3, c1, d1, e1, f1, g1 = 67.00, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
-b4, c2, d2, e2, f2, g2 = 106.00, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
+b3, c3, d3, e3, f3, g3 = 67.00, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
+b4, c4, d4, e4, f4, g4 = 106.00, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
 
 
 # User Input
-AVFST_Max_user = st.slider("AVFST_Max User Input", b3, b4, initial_values['AVFST_Max'])
-ARAIN_Average_user = st.slider("ARAIN_Average User Input", c1, c2, initial_values['ARAIN_Average'])
-HUC12_TN_user = st.slider("HUC12_TN User Input", d1, d2, initial_values['HUC12_TN'])
-HUC10_TP_user = st.slider("HUC10_TP User Input", e1, e2, initial_values['HUC10_TP'])
+AVFST_Max_user = st.slider("AVFST_Max User Input (°F)", b3, b4, initial_values['AVFST_Max'])
+ARAIN_Average_user = st.slider("ARAIN_Average User Input", c3, c4, initial_values['ARAIN_Average'])
+HUC12_TN_user = st.slider("HUC12_TN User Input", d3, d4, initial_values['HUC12_TN'])
+HUC10_TP_user = st.slider("HUC10_TP User Input", e3, e4, initial_values['HUC10_TP'])
 HUC10_cropland_area_user = st.slider("HUC10_cropland_area User Input", float(f1), float(f2), initial_values['HUC10_cropland_area_1'])
 HUC12_developed_area_5_user = st.slider("HUC12_developed_area_5 User Input", float(g1), float(g2), initial_values['HUC12_developed_area_5'])
 
