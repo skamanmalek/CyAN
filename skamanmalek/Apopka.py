3630643debc32709818dee29391bc054198f5178
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 21:42:23 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -93,5 +93,5 @@ chart_data = pd.DataFrame({
 })
 
 # Display the bar chart
-st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value',color=None, width=30, height=0, use_container_width=True)
+st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value',color=None, width=10, height=0, use_container_width=True)
 
