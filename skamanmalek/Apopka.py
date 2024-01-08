b6d21b8c1f2c227e27a3b6a36fbcfd2f4baa2348
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-07 19:48:53 -0500

    Update Apopka.py

--- skamanmalek/Apopka.py
+++ skamanmalek/Apopka.py
@@ -86,4 +86,5 @@ else:
 
 # Bar chart with thinner bars and color based on prediction
 bar_color = 'red' if percentage_change > 0 else 'green'
-st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value', color=bar_color, width=50)
+st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value', color=[bar_color], width=50)
+
