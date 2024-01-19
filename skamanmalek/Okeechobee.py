584eff55c48984a04a77566cce13aff103d5d994
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-19 06:31:07 -0500

    Update Okeechobee.py

--- skamanmalek/Okeechobee.py
+++ skamanmalek/Okeechobee.py
@@ -21,11 +21,11 @@ initial_values = {
 coefficients = {
     'intercept': 0.090361341,
     'AVFST_Max': 0.102975996,
-    'ARAIN_Average': 0.368312685,
-    'HUC12_TN': 0.266064535,
-    'HUC10_TP': 0.478937485,
+    'ARAIN_Average': 0.308312685,
+    'HUC12_TN': 0.206064535,
+    'HUC10_TP': 0.400937485,
     'HUC10_cropland_area_1': 0.30046161,
-    'HUC12_developed_area_5': 0.594119072
+    'HUC12_developed_area_5': 0.504119072
 }
 
 # Equations variables
