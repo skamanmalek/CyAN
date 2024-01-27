1bd1b22705c3c27e84842f79a5dcb08f1e91b6b7
skamanmalek <155853840+skamanmalek@users.noreply.github.com>
2024-01-27 06:36:33 -0500

    Update Winder.py

--- skamanmalek/Winder.py
+++ skamanmalek/Winder.py
@@ -5,59 +5,53 @@ import numpy as np
 # Display the title with blue color and centered text
 title_markdown = "<h1 style='color: blue; text-align: center;'>Future Cyanobacteria Bloom Magnitude Estimation in Lake Winder</h1>"
 st.markdown(title_markdown, unsafe_allow_html=True)
-
-# Initial values according to the baseline of 2022 for Lake Apopka
+# Data for Alligator Lake
 initial_values = {
-    'Norm_CyAN': 9.73828069,
+    'Norm_CyAN': 8.316145908,
     'AVFST_Max': 86.09,
     'ARAIN_Average': 212.67,
-    'HUC12_TN': 172.1270583,
-    'HUC10_TP': 18.03212517,
+    'HUC12_forest_and_shrubland_4': 1.058090542,
+    'HUC10_grassland_and_pasture_3': 32.71662666,
     'HUC10_cropland_area_1': 0.223292897,
     'HUC12_developed_area_5': 17.69361636
 }
 
-# Coefficients for Lake Apopka
 coefficients = {
-    'intercept': -0.111805244,
-    'AVFST_Max': -0.08491372,
-    'ARAIN_Average': 0.167654117,
-    'HUC12_TN': 0.050050587,
-    'HUC10_TP': -0.011830152,
-    'HUC10_cropland_area_1': -0.056150165,
-    'HUC12_developed_area_5': 0.173697977
+    'intercept': -0.015893139,
+    'AVFST_Max': -0.110293441,
+    'ARAIN_Average': 0.215613845,
+    'HUC12_forest_and_shrubland_4': 0.017667124,
+    'HUC10_grassland_and_pasture_3': -0.226728315,
+    'HUC10_cropland_area_1': -0.217765695,
+    'HUC12_developed_area_5': 0.113675518
 }
 
 # Equations variables
-b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 14.37253718, 7.105387318, 0, 0.052616068
-b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 252.0831295, 24.93183214, 86.75640259, 79.36556518
-
-
+b1, c1, d1, e1, f1, g1 = 82.04, 163.72, 0, 0, 0, 0.052616068
+b2, c2, d2, e2, f2, g2 = 90.86, 223.83, 80.3992991, 81.38497115, 86.75640259, 79.36556518
 
 # Sidebar for user inputs with icons
 st.sidebar.markdown("<h2 style='font-size: 24px;'>🛠️ User Inputs:</h2>", unsafe_allow_html=True)
 st.sidebar.write("The default values represent mean annual measurements derived from the 2022 baseline for Lake Winder.")
-
-
+ 
 # Slider variables:
 b3, c3, d3, e3, f3, g3 = 82.04, 0.00, 0.00000000, 0.000000000, 0, 0.000000000
-b4, c4, d4, e4, f4, g4 = 106.00, 450.00, 500.00, 50.00, 100.00, 100.00
+b4, c4, d4, e4, f4, g4 = 106.00, 450.00, 100.00, 100.00, 100.00, 100.00
 
 # User Input in the sidebar with colorful labels
-AVFST_Max_user = st.sidebar.slider("**🌡️ AVFST_Max_°F**", b3, b4, initial_values['AVFST_Max'], step=0.1, key="avfst_max", help="Adjust max air temperature.")
-ARAIN_Average_user = st.sidebar.slider("**🌧️ ARAIN_Average_kg/m^2**", c3, c4, initial_values['ARAIN_Average'], step=0.1, key="arain_average", help="Adjust average rainfall.")
-HUC12_TN_user = st.sidebar.slider("**🔍 HUC12_TN_mg/L**", d3, d4, initial_values['HUC12_TN'], step=0.1, key="huc12_tn", help="Adjust total nitrogen.")
-HUC10_TP_user = st.sidebar.slider("**📊 HUC10_TP_mg/L**", e3, e4, initial_values['HUC10_TP'], step=0.1, key="huc10_tp", help="Adjust total phosphorus.")
-HUC10_cropland_area_user = st.sidebar.slider("**🌱 HUC10_Cropland_Area_%**", float(f3), float(f4), initial_values['HUC10_cropland_area_1'], step=0.1, key="huc10_cropland", help="Adjust % cropland area.")
-HUC12_developed_area_5_user = st.sidebar.slider("**🏡 HUC12_Developed_Area_%**", float(g3), float(g4), initial_values['HUC12_developed_area_5'], step=0.1, key="huc12_developed", help="Adjust % developed area.")
-
+AVFST_Max_user = st.sidebar.slider("**🌡️ AVFST_Max_°F**", b3, b4, initial_values['AVFST_Max'], step=0.1, key="avfst_max", help="Adjust the annual maximum air temperature.")
+ARAIN_Average_user = st.sidebar.slider("**🌧️ ARAIN_Average_kg/m^2**", c3, c4, initial_values['ARAIN_Average'], step=0.1, key="arain_average", help="Adjust the annual average rainfall.")
+HUC12_forest_and_shrubland_4_user = st.sidebar.slider("**🌲 HUC12_Forest_and_Shrubland_%**", d3, d4, initial_values['HUC12_forest_and_shrubland_4'], step=0.1, key="huc12_forest_shrubland", help="Modify the percentage of forest and shrubland within the HUC12 watershed enclosing the lake.")
+HUC10_grassland_and_pasture_3_user = st.sidebar.slider("**🌾 HUC10_Grassland_and_Pasture_%**", e3, e4, initial_values['HUC10_grassland_and_pasture_3'], step=0.1, key="huc10_grassland_pasture", help="Modify the percentage of grassland and pasture within the HUC10 watershed enclosing the lake.")
+HUC10_cropland_area_user = st.sidebar.slider("**🌱 HUC10_Cropland_Area_%**", float(f3), float(f4), initial_values['HUC10_cropland_area_1'], step=0.1, key="huc10_cropland", help="Modify the percentage of cropland within the HUC10 watershed enclosing the lake.")
+HUC12_developed_area_5_user = st.sidebar.slider("**🏡 HUC12_Developed_Area_%**", float(g3), float(g4), initial_values['HUC12_developed_area_5'], step=0.1, key="huc12_developed", help="Modify the percentage of developed area within the HUC12 watershed enclosing the lake.")
 
 # Calculate Predicted Magnitude
 Y = coefficients['intercept'] + \
     coefficients['AVFST_Max'] * (AVFST_Max_user - b1) / (b2 - b1) + \
     coefficients['ARAIN_Average'] * (ARAIN_Average_user - c1) / (c2 - c1) + \
-    coefficients['HUC12_TN'] * (HUC12_TN_user - d1) / (d2 - d1) + \
-    coefficients['HUC10_TP'] * (HUC10_TP_user - e1) / (e2 - e1) + \
+    coefficients['HUC12_forest_and_shrubland_4'] * (HUC12_forest_and_shrubland_4_user - d1) / (d2 - d1) + \
+    coefficients['HUC10_grassland_and_pasture_3'] * (HUC10_grassland_and_pasture_3_user - e1) / (e2 - e1) + \
     coefficients['HUC10_cropland_area_1'] * (HUC10_cropland_area_user - f1) / (f2 - f1) + \
     coefficients['HUC12_developed_area_5'] * (HUC12_developed_area_5_user - g1) / (g2 - g1)
 
@@ -90,7 +84,6 @@ elif percentage_change > 0:
 else:
     st.success("**The annual magnitude of cyanobacteria bloom is predicted to decrease.**")
 
-
 # Bar chart
 chart_data = pd.DataFrame({
     'Magnitude Type': ['Initial Bloom Magnitude', 'Predicted Bloom Magnitude'],
@@ -99,6 +92,14 @@ chart_data = pd.DataFrame({
 
 # Display the bar chart
 st.bar_chart(chart_data, x='Magnitude Type', y='Magnitude Value')
+
+
+
+
+
+
+
+
 
 
 
